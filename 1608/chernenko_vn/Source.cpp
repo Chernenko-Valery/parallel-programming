20c9ef95a6f5488d06f1e49333463193ac808351
unknown <v02055020v@gmail.com>
2018-10-17 20:18:58 +0300

    Edit comments

--- 1608/chernenko_vn/Source.cpp
+++ 1608/chernenko_vn/Source.cpp
@@ -7,12 +7,12 @@
 using namespace std;
 
 
-//Укажите путь до файта input.txt
+//path to file input.txt
 char* path = "input.txt";
 
 int main(int argc, char **argv) {
 
-	//Запуск MPI
+	//Run MPI
 	int ProcRank, ProcSize;
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
@@ -20,10 +20,10 @@ int main(int argc, char **argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 
-	//Работа 0 Процесса
+	//Work 0 Process
 	if (ProcRank == 0) {
 
-		//Чтение массива из файла
+		//Read array from file
 		int x, y;
 		double times = MPI_Wtime();
 		FILE* fin;
@@ -37,20 +37,20 @@ int main(int argc, char **argv) {
 			for (int j = 0; j < y; j++) fscanf(fin, "%d ", arr + i*y + j);
 		fclose(fin);
 
-		//Если процесс всего 1
+		//If the process is only
 		if (ProcSize == 1) {
-			int *totalResult = (int*)malloc(y * sizeof(int)); //Строка результата
-			for (int i = 0; i < y; i++) totalResult[i] = INT_MIN; //"Обнуление" строки результата
+			int *totalResult = (int*)malloc(y * sizeof(int)); //Result string
+			for (int i = 0; i < y; i++) totalResult[i] = INT_MIN; //"Zeroing" the result string
 			for (int i = 0; i < x; i++) 
 				for (int j = 0; j < y; j++)
 					if (totalResult[j] < arr[i*y + j])
 						totalResult[j] = arr[i*y + j];
 
-			//Вывод
+			//Result
 			for (int i = 0; i < y; i++) printf("%d ", totalResult[i]);
 			printf("\nTime = %f\n", MPI_Wtime() - times);
 
-			//Освобождение памяти и окончание работы
+			//Memory release and end of work
 			free(totalResult);
 			free(arr);
 
@@ -58,29 +58,29 @@ int main(int argc, char **argv) {
 			return 0;
 		}
 
-		//Если не 1 процесс. 
-		//Передача информации о размерах матрицы
+		// If not 1 process.
+		// Transmission of information about the size of the matrix
 		for (int i = 1; i < ProcSize; i++) {
 			MPI_Send(&x, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 			MPI_Send(&y, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
 
-		//Вычисление количества строк для каждого процесса
+		// Calculate the number of rows for each process
 		int k = x / (ProcSize - 1);
 
-		//Отправка укороенной матрицы всем остальным процессам, кроме последнего
+		// Sending the shortened matrix to all other processes, except the last
 		for (int i = 1; i < ProcSize - 1; i++) {
 			MPI_Send(arr + (i-1)*k*y, k*y, MPI_INT, i, 0, MPI_COMM_WORLD);
 		}
-		//Отправление укороченной матрица последнему процессу
+		// Sending the shortened matrix to the last process
 		MPI_Send(arr + (ProcSize - 2)*k*y, (x - (ProcSize - 2)*k)*y, MPI_INT, ProcSize - 1, 0, MPI_COMM_WORLD);
 
-		//Создание временной и результирующей строки
+		// Create a temporary and result string
 		int* result = (int*)malloc(y * sizeof(int));
 		int *totalResult = (int*)malloc(y * sizeof(int));
 		for (int i = 0; i < y; i++)
 			totalResult[i] = INT_MIN;
-		//Получение информации от процессов
+		// Getting information from processes
 		for (int i = 1; i < ProcSize; i++) {
 			MPI_Recv(result, y, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			for (int j = 0; j < y; j++) 
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
 					totalResult[j] = result[j];
 		}
 
-		//Вывод и освобождение памяти.
+		//Memory release and end of work
 		for (int i = 0; i < y; i++) printf("%d ", totalResult[i]);
 		printf("\nTime = %f\n", MPI_Wtime() - times);
 
@@ -96,40 +96,40 @@ int main(int argc, char **argv) {
 		free(totalResult);
 		free(arr);
 	}
-	//Другие процессы
+	// Other processes
 	else {
-		//Получение размеров матрицы и количества строк на процесс
+		// Get the dimensions of the matrix and the number of rows per process
 		int x, y;
 		MPI_Recv(&x, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		MPI_Recv(&y, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		int k = x / (ProcSize - 1);
 
-		//Выделение памяти под укороченную матрицу
+		// Memory allocation for a shortened matrix
 		int* arr;
 		if( ProcRank != ProcSize - 1)
 			arr = (int*)malloc(y * k * sizeof(int));
 		else arr = (int*)malloc((x - (ProcSize - 2)*k) * y * sizeof(int));
 
-		//Выделение памяти под результат
+		// Memory allocation for the result
 		int* result = (int*)malloc(y * sizeof(int));
 		for (int i = 0; i < y; i++)
 			result[i] = INT_MIN;
 
-		//Вычисление результата для не последнего процесса
+		// Calculate the result for not the last process
 		if (ProcRank != ProcSize - 1) {
 			MPI_Recv(arr, k*y, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			for (int i = 0; i < k; i++)
 				for (int j = 0; j < y; j++)
 					if (result[j] < arr[i*y + j]) result[j] = arr[i*y + j];
 		}
-		//Вычисление результата для последнего процесса
+		// Calculate the result for the last process
 		else {
 			MPI_Recv(arr, (x - (ProcSize - 2)*k) * y, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			for (int i = 0; i < x - (ProcSize - 2)*k; i++)
 				for (int j = 0; j < y; j++)
 					if (result[j] < arr[i*y + j]) result[j] = arr[i*y + j];
 		}
-		//Отправка результата
+		// Send the result
 		MPI_Send(result, y, MPI_INT, 0, 0, MPI_COMM_WORLD);
 		free(result);
 		free(arr);
