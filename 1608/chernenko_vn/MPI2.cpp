cdc37e90334f8022ac94ce7f674f6f15e512fc11
Chernenko-Valery <v02055020v@gmail.com>
2018-11-15 19:40:27 +0300

    Заменены производственные типы на более подходящии, использован MPI_Scatterv и MPI_Reduce, используется меньше памяти, ускорена передача данных между процессорами, все еще долго)

--- 1608/chernenko_vn/MPI2.cpp
+++ 1608/chernenko_vn/MPI2.cpp
@@ -20,15 +20,41 @@ void PrintMatrix(int* arr, int x, int y) {
 	}
 }
 
-int* MultMatrOnVect(int* matrix, int x, int y, int* vector, int size) {
+//Output matrix to console
+void PrintMatrix(long* arr, int x, int y) {
+	for (int i = 0; i < x; i++) {
+		cout.width(4);
+		for (int j = 0; j < y; j++) cout << arr[i*y + j] << " ";
+		cout << endl;
+	}
+}
+
+//calculate all matrix
+long* MultMatrOnVect(int* matrix, int x, int y, int* vector, int size, int startIndex = 0) {
 	if (y != size) {
 		cout << "wrong format!" << endl;
 		return nullptr;
 	}
-	int *resultMatrix = new int[x] {0};
+	if (startIndex < 0 || startIndex > y) startIndex = 0;
+	long* resultMatrix = new long[x] {0};
 	for (int i = 0; i < x; i++)
-		for (int j = 0; j < y; j++)
+		for (int j = startIndex; j < y; j++)
 			resultMatrix[i] += matrix[i*y + j] * vector[j];
+	return resultMatrix;
+}
+
+//calculate patr of columns
+long* MultPartMatrOnVect(int* matrix, int x, int y, int* vector, int size, int startIndex, int countColumns) {
+	if (startIndex < 0 || startIndex > y) startIndex = 0;
+	if (countColumns < 0 || countColumns > y) countColumns = y;
+	if (countColumns != size) {
+		cout << "wrong format!" << endl;
+		return nullptr;
+	}
+	long* resultMatrix = new long[x] {0};
+	for (int i = 0; i < x; i++)
+		for (int j = startIndex; j < y; j++)
+			resultMatrix[i] += matrix[i*y + j] * vector[j - startIndex];
 	return resultMatrix;
 }
 
@@ -46,11 +72,17 @@ int main(int argc, char **argv)
 	MPI_Datatype columnsType;
 
 	if (ProcRank == 0) {
-
 		x = atoi(argv[1]);
 		y = atoi(argv[2]);
 		if (ProcSize == 1) k = 1;
 		else k = y / (ProcSize - 1);
+
+		//Create columnsType
+		int* sizes = new int[x] {x, y};
+		int* sizes2 = new int[x] {x, k};
+		int* starts = new int[x] {0, 0};
+		MPI_Type_create_subarray(2, sizes, sizes2, starts, MPI_ORDER_C, MPI_INT, &columnsType);
+		MPI_Type_commit(&columnsType);
 
 		MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&y, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -58,113 +90,106 @@ int main(int argc, char **argv)
 
 		//matrix creation
 		int* matrix = GetRandomMatrix(x, y, 10);
-//		cout << "Matrix =" << endl;
-//		PrintMatrix(matrix, x, y);
-//		cout << endl;
+
 		//vector creation
 		int* vector = GetRandomMatrix(y, 1, 10);
-//		cout << "Vector = " << endl;
-//		PrintMatrix(vector, y, 1);
-//		cout << endl;
 		
-		//Create columnsType
-		MPI_Type_vector(x, k, y, MPI_INT, &columnsType);
-		MPI_Type_commit(&columnsType);
+		long* resultVector = new long[x] {0};
 
-		//Start Time
 		double timeStart = MPI_Wtime();
 
 		//without parallelization
 		if (ProcSize == 1 || k < 2) {
-			int* ResultVector = MultMatrOnVect(matrix, x, y, vector, y);
+			long* resultVector = MultMatrOnVect(matrix, x, y, vector, y);
 			double timeEnd = MPI_Wtime();
 			cout << "Result = " << endl;
-			PrintMatrix(ResultVector, 1, x);
+			PrintMatrix(resultVector, 1, x);
 			cout << "Time = " << timeEnd - timeStart << endl;
 			delete matrix;
 			delete vector;
-			delete ResultVector;
+			delete resultVector;
+			MPI_Type_free(&columnsType);
 			MPI_Finalize();
 			return 0;
 		}
 
+		int* vectorsendcounts = new int[ProcSize];
+		int* vectordispls = new int[ProcSize];
+		for (int i = 0; i < ProcSize; i++) {
+			vectorsendcounts[i] = k;
+			vectordispls[i] = (i - 1)*k;
+		}
+		int lastY = (ProcSize - 1)*k;
+		int lastCount = y - lastY;
+		vectorsendcounts[0] = lastCount;
+		vectordispls[0] = lastY;
+		int* lastVector = new int[lastCount];
+		MPI_Scatterv(vector, vectorsendcounts, vectordispls, MPI_INT, lastVector, lastCount, MPI_INT, 0, MPI_COMM_WORLD);
 
-		int* ResultVector = new int[x] {0};
-		for (int i = 1; i < ProcSize; i++) {
-			int* buf = new int[x*k];
-			int* bufVect = new int[k];
-			for (int j = 0; j < k; j++) bufVect[j] = vector[(i - 1)*k + j];
-			int bufpos = 0;
-			MPI_Pack(matrix + (i - 1)*k, 1, columnsType, buf, x*k * sizeof(int), &bufpos, MPI_COMM_WORLD);
-			MPI_Send(buf, x*k, MPI_INT, i, 0, MPI_COMM_WORLD);
-			MPI_Send(bufVect, k, MPI_INT, i, 0, MPI_COMM_WORLD);
-			delete buf;
-			delete bufVect;
-			}
-		//count last columns
-		if ((ProcSize - 1) *k != y) {
-			MPI_Datatype lastColumn;
-			int lastY = y - (ProcSize - 1)*k;
-			MPI_Type_vector(x, lastY, y, MPI_INT, &lastColumn);
-			MPI_Type_commit(&lastColumn);
-			int* lastMatrix = new int[x*lastY];
-			int* lastVector = new int[lastY];
-			for (int i = 0; i < lastY; i++) lastVector[i] = vector[(ProcSize - 1)*k + i];
-			int lastMatrixPos = 0;
-			MPI_Pack(matrix + (ProcSize - 1)*k, 1, lastColumn, lastMatrix, x * lastY * sizeof(int), &lastMatrixPos, MPI_COMM_WORLD);
-			MPI_Type_free(&lastColumn);
-			delete lastMatrix;
-			delete lastVector;
-			ResultVector = MultMatrOnVect(lastMatrix, x, lastY, lastVector, lastY);
+		long* tmpResultVector;
+		if (lastCount != 0) {
+			tmpResultVector = MultPartMatrOnVect(matrix, x, y, lastVector, lastCount, lastY, lastCount);
 		}
-		int* tmpResult = new int[x];
+		else tmpResultVector = new long[x] {0};
+
 		for (int i = 1; i < ProcSize; i++) {
-			MPI_Recv(tmpResult, x, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-			for (int i = 0; i < x; i++) ResultVector[i] += tmpResult[i];
+			MPI_Send(matrix + (i - 1)*k, 1, columnsType, i, 0, MPI_COMM_WORLD);
 		}
-		delete tmpResult;
+
+
+		MPI_Reduce(tmpResultVector, resultVector, x, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
 		double timeEnd = MPI_Wtime();
 		cout << "Result = ";
-		PrintMatrix(ResultVector, 1, x);
+		PrintMatrix(resultVector, 1, x);
 		cout << "Time with parallelization= " << timeEnd - timeStart << endl;
 
 		//Check
 		bool check = true;
 		double checkTimeStart = MPI_Wtime();
-		int* checkResult = MultMatrOnVect(matrix, x, y, vector, y);
+		long* checkResult = MultMatrOnVect(matrix, x, y, vector, y);
 		double checkTimeEnd = MPI_Wtime();
-		for (int i = 0; i < x; i++) if (checkResult[i] != ResultVector[i]) check = false;
-		if (check = true)
+		for (int i = 0; i < x; i++) if (checkResult[i] != resultVector[i]) check = false;
+		if (check == true)
 			cout << "Check success!" << endl;
 		else cout << "Encorrect answer!" << endl;
 		cout << "Time without parallelization= " << checkTimeEnd - checkTimeStart << endl;
 
-		MPI_Type_free(&columnsType);
 		delete matrix;
 		delete vector;
-		delete ResultVector;
+		delete tmpResultVector;
+		delete resultVector;
 		delete checkResult;
 
 	} else {
+
 		MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&y, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&k, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+		int* sizes = new int[x] {x, y};
+		int* sizes2 = new int[x] {x, k};
+		int* starts = new int[x] {0, 0};
+		MPI_Type_create_subarray(2, sizes, sizes2, starts, MPI_ORDER_FORTRAN, MPI_INT, &columnsType);
+		MPI_Type_commit(&columnsType);
+
 		if (k > 1) {
-			int* matrix = new int[x*k];
+			int* matrix = new int[x*k]{0};
 			int* vector = new int[k];
-			MPI_Recv(matrix, x*k, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-			MPI_Recv(vector, k, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+			MPI_Scatterv(nullptr, nullptr, nullptr, MPI_INT, vector, k, MPI_INT, 0, MPI_COMM_WORLD);
+			MPI_Recv(matrix, 1, columnsType, 0, 0, MPI_COMM_WORLD, &status);
 			double timeStart = MPI_Wtime();
-			int* resultVector = MultMatrOnVect(matrix, x, k, vector, k);
+			long* resultVector = MultMatrOnVect(matrix, x, k, vector, k);
 			double timeEnd = MPI_Wtime();
-			MPI_Send(resultVector, x, MPI_INT, 0, 0, MPI_COMM_WORLD);
-			cout << "Time on Proc" << ProcRank << " = " << timeEnd - timeStart << endl;
+			MPI_Reduce(resultVector, nullptr, x, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+			cout << "Time on Proc " << ProcRank << " = " << timeEnd - timeStart << endl;
 			delete matrix;
 			delete vector;
 			delete resultVector;
+
 		}
 	}
-
+	MPI_Type_free(&columnsType);
 	MPI_Finalize();
 	return 0;
 
